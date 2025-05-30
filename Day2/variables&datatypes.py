97a273b32d67db8db58820ac35ed73130f77526e
RRRashila <118243617+RRRashila@users.noreply.github.com>
2025-05-30 04:35:43 +0000

    Variables and datatypes

---
+++ Day2/variables&datatypes.py
@@ -0,0 +1,12 @@
+'''
+ Topics
+- Integers, floats, strings, booleans, type conversion
+
+ Challenge
+- Calculate the area of a rectangle using user-input length and width
+'''
+print("To find the area of a rectangle")
+l = float(input("Enter the length of a rectangle : "))
+w = float(input("Enter the width of a rectangle: "))
+area = l * w
+print(f"The area of a rectangle is : {area}")
